2aa54d1e864ea185fe54114d5fcb8d1f0e3336d3
mohamadmasmas <masmasmohamad@gmail.com>
2024-08-21 20:08:52 -0400

    Finished creating routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -188,7 +188,7 @@ class TestAccountService(TestCase):
         new_count = self.get_account_count()
         self.assertEqual(new_count, account_count - 1)
 
-    # Test Read Account
+    # Test Read Account 
     def test_get_account(self):
         """It should Get a single Product"""
         # get the id of a an account
